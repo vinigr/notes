4415002c789c42d929b30b1f1f88ae313b94775c
Vinícios Grama <vinyirun1@gmail.com>
2020-01-13 23:09:07 -0300

    environment ready for new testing

--- packages/server/test/createResource/createRows.ts
+++ packages/server/test/createResource/createRows.ts
@@ -1,8 +1,9 @@
 // @flow
 /* eslint-disable no-multi-assign,prefer-const */
 
-import { User } from '../../src/models';
+import { User, Category } from '../../src/models';
 import { IUser } from '../../src/modules/user/UserModel';
+import { ICategory } from '../../src/modules/category/CategoryModel';
 
 export const restartCounters = () => {
   global.__COUNTERS__ = Object.keys(global.__COUNTERS__).reduce((prev, curr) => ({ ...prev, [curr]: 0 }), {});
@@ -16,6 +17,15 @@ export const createUser = async (payload: Partial<IUser> = {}) => {
     email: `user-${n}@example.com`,
     password: '123456',
     active: true,
+    ...payload,
+  }).save();
+};
+
+export const createCategory = async (payload: Partial<ICategory> = {}) => {
+  const n = (global.__COUNTERS__.category += 1);
+
+  return new Category({
+    name: `Category ${n}`,
     ...payload,
   }).save();
 };
