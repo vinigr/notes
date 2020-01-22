8068abdec5c1edd233c758c17da7feb7a1b10ade
Vinícios Grama <vinyirun1@gmail.com>
2020-01-22 15:35:31 -0300

    update index file react

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,11 +2,21 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+
+import { Environment } from '@todo/relay-web';
+
+import App from './App';
 
 const rootEl = document.getElementById('root');
 
 if (rootEl) {
-  ReactDOM.render(<h1>Hello</h1>, rootEl);
+  ReactDOM.render(
+    <RelayEnvironmentProvider environment={Environment}>
+      <App />
+    </RelayEnvironmentProvider>,
+    rootEl,
+  );
 } else {
   throw new Error('wrong rootEl');
 }
