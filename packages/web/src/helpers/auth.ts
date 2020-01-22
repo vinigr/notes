21018a61721d37ea98d081da0a4187713b5a0dd6
Vinícios Grama <vinyirun1@gmail.com>
2020-01-22 15:33:57 -0300

    create helper to auth

---
+++ packages/web/src/helpers/auth.ts
@@ -0,0 +1,24 @@
+import decode from 'jwt-decode';
+
+const ACCESS_TOKEN = 'NOTE_APP';
+
+export const login = (accessToken, remember) =>
+  remember ? localStorage.setItem(ACCESS_TOKEN, accessToken) : sessionStorage.setItem(ACCESS_TOKEN, accessToken);
+
+export const isLoggedIn = () => {
+  const token = getAccessToken();
+  return !!token && !isTokenExpired(token);
+};
+
+export const getAccessToken = () => localStorage.getItem(ACCESS_TOKEN) || sessionStorage.getItem(ACCESS_TOKEN);
+
+const isTokenExpired = token => {
+  try {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      return true;
+    } else return false;
+  } catch (err) {
+    return false;
+  }
+};
