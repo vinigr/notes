8a1712344a4f7e7680d71d7b226b7049bcbe5a3b
Vinícios Grama <vinyirun1@gmail.com>
2020-01-27 19:39:02 -0300

    add new routes

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import Loading from './components/Loading';
 
 import AuthRoute from './components/AuthRoute';
+import PrivateRoute from './components/PrivateRoute';
 
-import { ROUTE_LOGIN, ROUTE_REGISTER } from './helpers/constants';
+import { ROUTE_LOGIN, ROUTE_REGISTER, ROUTE_HOME, ROUTE_NOTE } from './helpers/constants';
 import { GlobalStyle } from './styles/global';
 
 const Login = lazy(() => import('./pages/Login/Login'));
 const Register = lazy(() => import('./pages/Register/Register'));
+const Notes = lazy(() => import('./pages/Notes/Notes'));
+const Note = lazy(() => import('./pages/Note/Note'));
 
 const App: React.FC = () => {
   return (
@@ -18,6 +21,8 @@ const App: React.FC = () => {
         <Switch>
           <AuthRoute path={ROUTE_LOGIN} component={Login} />
           <AuthRoute path={ROUTE_REGISTER} component={Register} />
+          <PrivateRoute path={ROUTE_HOME} component={Notes} />
+          <PrivateRoute path={ROUTE_NOTE} component={Note} />
         </Switch>
       </Suspense>
       <GlobalStyle />
