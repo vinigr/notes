89e0ebfcbbbe99bdc9fb99c61f267d5226218451
Vinícios Grama <vinyirun1@gmail.com>
2020-01-27 19:38:53 -0300

    update constants

--- packages/web/src/helpers/constants.ts
+++ packages/web/src/helpers/constants.ts
@@ -3,4 +3,6 @@
 export const ROUTE_LOGIN = '/login';
 export const ROUTE_REGISTER = '/register';
 
-export const ROUTE_HOME = '/home';
+export const ROUTE_HOME = '/notes';
+
+export const ROUTE_NOTE = '/note/:id';
