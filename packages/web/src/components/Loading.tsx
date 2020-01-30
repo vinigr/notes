b546f0e584f7d30c850f18d34bc81357b4f4b8f3
Vinícios Grama <vinyirun1@gmail.com>
2020-01-29 22:35:18 -0300

    update loading component

--- packages/web/src/components/Loading.tsx
+++ packages/web/src/components/Loading.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const Loader = styled.div`
+  font-size: 10px;
+  margin: 50px auto;
+  text-indent: -9999em;
+  width: 5em;
+  height: 5em;
+  border-radius: 50%;
+  background: #548596;
+  background: linear-gradient(to right, #548596 10%, rgba(64, 128, 128, 0) 42%);
+  position: relative;
+  -webkit-animation: load3 1.4s infinite linear;
+  animation: load3 1.4s infinite linear;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+
+  &::before {
+    width: 50%;
+    height: 50%;
+    background: #548596;
+    border-radius: 100% 0 0 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: '';
+  }
+
+  &::after {
+    background: #fff;
+    width: 75%;
+    height: 75%;
+    border-radius: 50%;
+    content: '';
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+  }
+
+  @-webkit-keyframes load3 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes load3 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 const Loading: React.FC = () => {
-  return <span>Loading...</span>;
+  return <Loader>Loading...</Loader>;
 };
 
 export default Loading;
